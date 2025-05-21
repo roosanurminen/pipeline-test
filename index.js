38efe4535ab4559fc09ad5ef52ad7cd6bb45e835
Roosa Nurminen <roosa.nurminen@tuni.fi>
2025-05-21 20:53:45 +0300

    Change code for testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello :D</h1>')
+  res.send('<h1>Testi 2 (varmuudeksi :D)</h1>')
 })
 
 const PORT = 8080
